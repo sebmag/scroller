86cd85ad33625d76f15eda984cbf5d618199b2ea
sebmag <sebmaginfo@free.fr>
2016-12-02 10:57:43 +0100

    Fichiers scroller.js et scroller.min.js modifiés

--- scroller.js
+++ scroller.js
@@ -2,12 +2,22 @@
 // Fonction scroller : Permet de faire défiler la page progressivement jusqu'à un élément.
 //********************************************************************************************
 
-function scroller(origine, cible, duree = 1000){
-    var departElmt = $(origine);
-    var arriveeElmt = $(cible);
-    departElmt.click(function(){
-    $('html, body').animate({
-        scrollTop:arriveeElmt.offset().top
+function scroller(origine, cible, duree){
+    
+    if(duree === 'undefined') {
+        duree = 1000;
+    }
+    if(cible === 'undefined') {
+        cible = 'body';
+    }
+    
+    var origineElmt = $(origine);
+    var cibleElmt = $(cible);
+    
+    origineElmt.click(function(){
+    $('html, body').animate({ 
+    scrollTop:cibleElmt.offset().top 
     }, duree);
+    
     return false;});
 }
